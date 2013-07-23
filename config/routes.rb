9cf6f5d1b13389fa7f5dd1a108419623c4ef3d75
Neeraj <neeraj.singh@60degree.com>
2013-07-23 19:14:23 +0530

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,12 @@ SampleApp::Application.routes.draw do
   # root 'welcome#index'
 
   root  'static_pages#home'
+  resources :users
 
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
 
-  get "users/new"
   match '/signup',  to: 'users#new',            via: 'get'
 
   # Example of regular route:
